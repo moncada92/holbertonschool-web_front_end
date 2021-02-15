1f2fe08ba02bf4961073d92226019068a2df2820
Camilo Moncada <monk-da@hotmail.com>
2021-02-15 12:47:42 -0500

    lexical scope question 0

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,11 @@
+function welcome(firstName, lastName) {
+  let fullName = `${firstName} ${lastName}`;
+
+  function displayFullName(){
+    alert(`welcome ${fullName}!`);
+  }
+
+  displayFullName();
+}
+
+welcome('Holberton', 'School');
